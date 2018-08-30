7ef048253bd6dd1f76ed8a41976374d30a73e044
Andy Pan <panjf2000@gmail.com>
2018-08-30 23:02:20 +0800

    add config to factory

--- factory/factory.go
+++ factory/factory.go
@@ -2,9 +2,12 @@ package factory
 
 import (
 	"github.com/panjf2000/loong/brokers"
+	"github.com/panjf2000/loong/config"
 )
 
 type BrokerFactory struct {
+	brokerConf config.BrokerConfig
+	backendConf config.BrokerConfig
 }
 
 func (bf BrokerFactory) ProduceBroker() (brokers.StandardBroker, error) {
