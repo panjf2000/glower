a5799e5409c096b30e7c9c9279347f450f637f66
Andy Pan <panjf2000@gmail.com>
2018-09-15 22:07:09 +0800

    update

--- protocol/proto.go
+++ protocol/proto.go
@@ -1,11 +1,22 @@
 package protocol
 
-type Protocol struct {
+import "time"
+
+type Params struct {
+	Type  string
+	Value interface{}
 }
 
+type Protocol map[string]interface{}
+
 type ProtoMessage struct {
+	UUID       string
+	Name       string
+	RoutingKey string
+	ETA        *time.Time
+	Headers    Protocol
+	params     []Params
 }
 
 type ResultMessage struct {
 }
-
