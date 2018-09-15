a8be0c8d4a6eb4d7862d51c0234f94d2a2dd9040
Andy Pan <panjf2000@gmail.com>
2018-09-15 22:08:00 +0800

    update

--- brokers/broker.go
+++ brokers/broker.go
@@ -2,10 +2,9 @@ package brokers
 
 import (
 	"github.com/panjf2000/loong/protocol"
-	"github.com/panjf2000/loong/config"
 )
 
 type StandardBroker interface {
-	Publish(proto *protocol.Protocol) error
-	Receive() (protocol.ProtoMessage, error)
+	Publish(message protocol.ProtoMessage) error
+	Receive() (protocol.ResultMessage, error)
 }
